df9db4606bf4ff698e662d56ecd73b1391e37b2f
Rodrigo Gaiga <114424986+rgaiga@users.noreply.github.com>
2025-07-19 14:28:37 -0300

    feat: chapter 15: add binary operators

--- src/main.c
+++ src/main.c
@@ -9,9 +9,18 @@ int main(int argc, const char *argv[]) {
     Chunk chunk;
     init_chunk(&chunk);
 
-    write_constant(&chunk, 155.5, 122);
-    write_chunk(&chunk, OP_NEGATE, 123);
-    write_chunk(&chunk, OP_RETURN, 124);
+    // 1.2 + 3.4
+    write_constant(&chunk, 1.2, 10);
+    write_constant(&chunk, 3.4, 11);
+    write_chunk(&chunk, OP_ADD, 12);
+
+    // / 5.6
+    write_constant(&chunk, 5.6, 13);
+    write_chunk(&chunk, OP_DIVIDE, 14);
+
+    // return -result
+    write_chunk(&chunk, OP_NEGATE, 15);
+    write_chunk(&chunk, OP_RETURN, 16);
 
     disassemble_chunk(&chunk, "test chunk");
     interpret(&chunk);
